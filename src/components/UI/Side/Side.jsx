4f32f108a3b725bd808d4a6fd9938fe247dc49e9
igorwessel <igor_wessel@hotmail.com>
2020-03-01 08:29:25 -0300

    Finished header for component side.

--- src/components/UI/Side/Side.jsx
+++ src/components/UI/Side/Side.jsx
@@ -4,18 +4,22 @@ import Button from '../../Button/Button'
 import iconStatus from '../../Button/images/icon-status.png'
 import iconMessages from '../../Button/images/icon-messages.png'
 import iconThreePoints from '../../Button/images/icon-three-points.png'
+import avatarExample from '../../Button/images/example-avatar.jpeg'
 
 
 const Side = props => {
     return (
         <div className="side">
-            <div className="side-header">
-                <div className="side-container-buttons">
-                    <Button iconImg={iconStatus} btnImgDesc='status'/>
-                    <Button iconImg={iconMessages} btnImgDesc='new message'/>
-                    <Button iconImg={iconThreePoints} btnImgDesc='more options'/>
+            <header className='side-header'>
+                <div className="side-avatar">
+                    <Button iconImg={avatarExample} iconDesc='avatar'/> 
                 </div>
-            </div>
+                <div className="side-container-buttons">
+                    <Button iconImg={iconStatus} iconDesc='status'/>
+                    <Button iconImg={iconMessages} iconDesc='new message'/>
+                    <Button iconImg={iconThreePoints} iconDesc='more options'/>
+                </div>
+            </header>
         </div>
     )
 }
