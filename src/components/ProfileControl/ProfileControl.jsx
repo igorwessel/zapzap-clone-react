e1b3801b91d3ccaa9a9614b0993d6db5c5cb5dbf
igorwessel <igor_wessel@hotmail.com>
2020-03-02 09:05:41 -0300

    Fix references in import.

--- src/components/ProfileControl/ProfileControl.jsx
+++ src/components/ProfileControl/ProfileControl.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import Anime from 'react-anime';
 import './ProfileControl.css'
-import avatarExample from '../Button/images/example-avatar.jpeg'
- 
+import avatarExample from '../UI/Button/images/example-avatar.jpeg'
 
 
-class ProfileControl extends React.Component{
 
-    render(){
-        return(
-            <Anime width={['0', '100%']}>
+class ProfileControl extends React.Component {
+
+    render() {
+        return (
+            <Anime width={['0', '100%']} >
                 <div className="side-profile">
                     <header>
-                        <i className="fas fa-arrow-left" 
+                        <i className="fas fa-arrow-left"
                             onClick={this.props.clickHideOverlay}></i>
                         <h3>Perfil</h3>
                     </header>
                     <div className="side-profile-details">
                         <div className="side-profile-details-image">
                             <Anime scale={[.5, .9]} delay={100}>
-                                <img src={avatarExample} alt=""/>
+                                <img src={avatarExample} alt="" />
                             </Anime>
                         </div>
                         <div className="side-profile-details-name">
--- src/components/UI/Side/Side.jsx
+++ src/components/UI/Side/Side.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import './Side.css'
 import Button from '../Button/Button'
 import Input from '../Input/Input'
-import iconStatus from '../../Button/images/icon-status.png'
-import iconMessages from '../../Button/images/icon-messages.png'
-import iconThreePoints from '../../Button/images/icon-three-points.png'
-import avatarExample from '../../Button/images/example-avatar.jpeg'
+import iconStatus from '../Button/images/icon-status.png'
+import iconMessages from '../Button/images/icon-messages.png'
+import iconThreePoints from '../Button/images/icon-three-points.png'
+import avatarExample from '../Button/images/example-avatar.jpeg'
 import MessagePreview from '../../Message/MessagePreview/MessagePreview'
 import ProfileControl from '../../ProfileControl/ProfileControl'
 
 
 class Side extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             profileControl: false,
@@ -34,69 +34,69 @@ class Side extends React.Component {
     }
 
 
-    render(){
+    render() {
         const clickedinProfile = this.state.profileControl
         let profileControl;
-        
-        if (clickedinProfile){
-            profileControl = <ProfileControl clickHideOverlay={this.handleProfileClickHide}/>
+
+        if (clickedinProfile) {
+            profileControl = <ProfileControl clickHideOverlay={this.handleProfileClickHide} />
             document.querySelector('.side').classList.add('none')
         }
 
 
 
-        return(
+        return (
             <React.Fragment>
-            {profileControl}
-            <div className="side">
-                <header className='side-header'>
-                    <div className="side-avatar">
-                        <Button iconImg={avatarExample} 
+                {profileControl}
+                <div className="side">
+                    <header className='side-header'>
+                        <div className="side-avatar">
+                            <Button iconImg={avatarExample}
                                 iconDesc='avatar'
                                 iconId='avatarImg'
-                                click={this.handleProfileClick}/> 
-                    </div>
-                    <div className="side-container-buttons">
-                        <Button iconImg={iconStatus} 
-                                iconDesc='status' 
+                                click={this.handleProfileClick} />
+                        </div>
+                        <div className="side-container-buttons">
+                            <Button iconImg={iconStatus}
+                                iconDesc='status'
                                 iconId='status' />
-                        <Button iconImg={iconMessages} 
+                            <Button iconImg={iconMessages}
                                 iconDesc='new message'
-                                iconId='newMessage'/>
-                        <Button iconImg={iconThreePoints} 
+                                iconId='newMessage' />
+                            <Button iconImg={iconThreePoints}
                                 iconDesc='more options'
-                                iconId='moreOptions'/>
+                                iconId='moreOptions' />
+                        </div>
+                    </header>
+                    <div className="side-container-input">
+                        <i className="fas fa-search"></i>
+                        <Input placeholder='Procurar ou comecar uma nova conversa.' />
                     </div>
-                </header>
-                <div className="side-container-input">
-                    <i className="fas fa-search"></i>
-                    <Input placeholder='Procurar ou comecar uma nova conversa.'/>
+                    <div className="messages-previews">
+                        <MessagePreview
+                            avatarImg={avatarExample}
+                            authorMessage='Igor'
+                            dateMessage={'Hoje'}
+                            message={'Opa!'} />
+                        <MessagePreview
+                            avatarImg={avatarExample}
+                            authorMessage='Igor'
+                            dateMessage={'Ontem'}
+                            message={'Opa!'} />
+                        <MessagePreview avatarImg={avatarExample}
+                            authorMessage='Igor'
+                            dateMessage={'01/03/2020'}
+                            message={'Opa!'} />
+                        <MessagePreview avatarImg={avatarExample}
+                            authorMessage='Igor'
+                            dateMessage={'Hoje'}
+                            message={'Bom dia!'} />
+                        <MessagePreview avatarImg={avatarExample}
+                            authorMessage='Igor'
+                            dateMessage={'Hoje'}
+                            message={'Lorem ipsum upsum ipsun ipsun ipsun ipsun ipsun ipsun ipsun!'} />
+                    </div>
                 </div>
-                <div className="messages-previews">
-                    <MessagePreview 
-                        avatarImg={avatarExample} 
-                        authorMessage='Igor' 
-                        dateMessage={'Hoje'} 
-                        message={'Opa!'} />
-                    <MessagePreview 
-                        avatarImg={avatarExample} 
-                        authorMessage='Igor' 
-                        dateMessage={'Ontem'} 
-                        message={'Opa!'} />
-                    <MessagePreview avatarImg={avatarExample} 
-                        authorMessage='Igor' 
-                        dateMessage={'01/03/2020'} 
-                        message={'Opa!'} />
-                    <MessagePreview avatarImg={avatarExample} 
-                        authorMessage='Igor' 
-                        dateMessage={'Hoje'} 
-                        message={'Bom dia!'} />
-                    <MessagePreview avatarImg={avatarExample} 
-                        authorMessage='Igor' 
-                        dateMessage={'Hoje'} 
-                        message={'Lorem ipsum upsum ipsun ipsun ipsun ipsun ipsun ipsun ipsun!'} />
-                </div>
-            </div>
             </React.Fragment>
         );
     }
