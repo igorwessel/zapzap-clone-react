435c77f911a3b7d6076e20db7cc84f81d41bcbb8
igorwessel <igor_wessel@hotmail.com>
2020-03-28 10:59:33 -0300

    refactoring: Redoing everything.

---
+++ src/components/Input/index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+import styled from 'styled-components'
+
+const Input = styled.input`
+    padding: 15px;
+    text-align: center;
+    width: 150px;
+`
+
+export default Input
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Input from 'components/Input'
+
+
+const App = (props) => {
+    return(
+        <Input />
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+)
