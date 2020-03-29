d3904c22fbf291c94e42888c13306cfe2679bf98
igorwessel <igor_wessel@hotmail.com>
2020-03-29 08:59:12 -0300

    feat: Finish component Chat.

---
+++ src/components/Chat/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import styles from './styles.module.css'
+import defaultProfileImg from 'assets/default-user-image.png'
+import { FiMoreVertical, FiSearch, FiPaperclip } from 'react-icons/fi'
+import { FaMicrophone, FaRegSmileBeam } from 'react-icons/fa'
+
+const Chat = props => (
+    <div className={styles.container}>
+        <header className={styles.header}>
+            <div className={styles.friend}>
+                <img src={defaultProfileImg} alt="User Img"/>
+                <div>
+                    <h3>Amigo</h3>
+                    <span>visto por ...</span>
+                </div>
+            </div>
+            <div className={styles.headerIcons}>
+                <FiSearch size={22} color="rgba(0, 0, 0, .5)"/>
+                <FiPaperclip size={22} color="rgba(0, 0, 0, .5)"/>
+                <FiMoreVertical size={22} color="rgba(0, 0, 0, .5)"/>
+            </div>
+        </header>
+
+        <div className={styles.chat}>
+            {/* Enter component message here */}
+        </div>
+        
+        <div className={styles.header}>
+            <FaRegSmileBeam size={24} color="rgba(0, 0, 0, .5)"/>
+            <input type="text"/>
+            <FaMicrophone size={24} color="rgba(0, 0, 0, .5)"/>
+        </div>
+    </div>
+)
+
+export default Chat
--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,29 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FiArrowLeft } from 'react-icons/fi'
 import { MdEdit } from 'react-icons/md'
 
 import styles from './styles.module.css'
 import defaultProfileImg from 'assets/default-user-image.png'
 
-const Profile = props => (
-    <div className={styles.container}>
-        <header className={styles.header}>
-            <FiArrowLeft size={24}/>
-            <span>Perfil</span>
-        </header>
-        <div className={styles.photo}>
-            <img src={defaultProfileImg} alt="User Profile"/>
+const Profile = props => {
+    const [name, setName] = useState('Contato')
+
+    return(
+        <div className={styles.container}>
+            <header className={styles.header}>
+                <FiArrowLeft size={24}/>
+                <span>Perfil</span>
+            </header>
+
+            <div className={styles.photo}>
+                <img src={defaultProfileImg} alt="User Profile"/>
+            </div>
+
+            <section className={styles.form}>
+                <form>
+                    <label htmlFor="display-name">Nome</label>
+                    <div>
+                        <input 
+                            type="text" 
+                            name="display-name" 
+                            value={name}
+                            onChange={e => setName(e.target.value)}/>
+                        <MdEdit size={24} color="#919191"/>
+                    </div>
+                </form>
+            </section>
         </div>
-        <section className={styles.form}>
-            <form>
-                <label htmlFor="display-name">Nome</label>
-                <div>
-                    <input type="text" name="display-name" value="Contato"/>
-                    <MdEdit size={24} color="#919191"/>
-                </div>
-            </form>
-        </section>
-    </div>
-)
+    )
+}
 
 export default Profile
